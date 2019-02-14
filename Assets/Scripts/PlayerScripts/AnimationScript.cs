954ffbb1557d429c3ff6e818fd53e01aab933fd9
ItsStanMarsh <35196896+ItsStanMarsh@users.noreply.github.com>
2019-02-14 10:50:05 +0100

    animation for so far its done

---
+++ Assets/Scripts/PlayerScripts/AnimationScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript: MonoBehaviour {
+
+    public Animator anim;
+    public SpriteRenderer spriterenderer;
+    public float xInput;
+
+    private bool dead = false;
+
+
+    void Update () {
+
+        
+        if (!dead)
+            dead = Input.GetButton("KYS");
+
+        if (!dead)
+        {
+            Movement();
+        }
+        else anim.SetBool("isDead", true);
+
+    }
+
+  
+
+    public void Movement()
+    {
+
+        xInput = Input.GetAxisRaw("Horizontal");
+       
+            if (xInput == 1 || xInput == -1)
+            {
+                anim.SetInteger("Speed", 1);
+            }
+            else anim.SetInteger("Speed", 0);
+        
+    }
+
+}
